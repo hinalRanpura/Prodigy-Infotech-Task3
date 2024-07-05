38a4ad2273b98fd104dc2f3f57b8c26b9adb5172
hinalRanpura <134267119+hinalRanpura@users.noreply.github.com>
2024-07-05 22:34:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Timer from './components/Timer';
+import H from "./components/h";
+function App() {
+  return (
+    <div className="App">
+     {/* <Timer/> */}
+     <H />
+    </div>
+  );
+}
+
+export default App;
